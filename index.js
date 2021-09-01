d3bb25f5f69849860bd255107036a8d6bf472849
kalyani joshi <kalyani121@gmail.com>
2021-09-01 17:13:03 -0400

    heroku port changes

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
-app.listen(PORT,  () =>
+app.listen(process.env.PORT || PORT,  () =>
 console.log(`Example app listening at http://localhost:${PORT}`)
 );
